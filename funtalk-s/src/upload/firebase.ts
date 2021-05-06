cd8ada90cc7f1f65f3e36352775a9fdee741fcfd
Anderson <chltjdrhd777@naver.com>
2021-05-06 10:10:14 +0900

    chore : store firebase keys into .env

---
+++ funtalk-s/src/upload/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.F_APIKEY,
+    authDomain: process.env.F_DOMAIN,
+    projectId: process.env.F_PROJECTID,
+    storageBucket: process.env.F_STORAGEBUKKET,
+    messagingSenderId: process.env.F_MESSAGINGID,
+    appId: process.env.F_APPID,
+    measurementId: process.env.F_MEASURMENT
+};
+
+const init = firebase.initializeApp(firebaseConfig);
+export const db = init.firestore();
+export const ref = db.collection('test');
